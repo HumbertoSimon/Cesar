075b51b8468de886479e90304b3b266a1030f540
HumbertoSimon <humbertosimon08@aragon.unam.mx>
2022-10-09 23:19:40 -0500

    Cesar

--- src/main/java/Deep.java
+++ src/main/java/Deep.java
@@ -3,32 +3,32 @@ public class Deep {
     public static void main(String[] args) {
         
         
-        String Ab = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
-		String Texto = "Si Cupdmzaplil Uikpwuis Icbwuwti lm Tmfpkw oi lmamtxmvilw cu xixms xzwbiñwupkw mu si opabwzpi g mu si nwztikpwu lm ucmabzw xipa. Sia bizmia acabiubpdia lm mabi puabpbckpwu xcjspki, icbwuwti g sipki awu si lwkmukpi, si pudmabpñikpwu g si lpncapwu lm si kcsbczi.\r\n"
+        String assassins = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+		String code = "Si Cupdmzaplil Uikpwuis Icbwuwti lm Tmfpkw oi lmamtxmvilw cu xixms xzwbiñwupkw mu si opabwzpi g mu si nwztikpwu lm ucmabzw xipa. Sia bizmia acabiubpdia lm mabi puabpbckpwu xcjspki, icbwuwti g sipki awu si lwkmukpi, si pudmabpñikpwu g si lpncapwu lm si kcsbczi.\r\n"
 				+ "Mu ms tculw ikilmtpkw ma zmkwuwkpli kwtw cui cupdmzaplil lm mfkmsmukpi. Si CUIT zmaxwulm is xzmamubm g tpzi ms ncbczw kwtw ms xzwgmkbw kcsbczis tia ptxwzbiubm lm Tmfpkw.\r\n"
 				+ "Si CUIT ma cu maxikpw lm spjmzbilma. Mu mssi am xzikbpki kwbplpiuitmubm ms zmaxmbw, si bwsmziukpi g ms lpiswñw. Si xsczisplil lm plmia g lm xmuaitpmubw ma ixzmkpili kwtw apñuw lm ac zpycmhi g ucuki kwtw nikbwz lm lmjpsplil.";
-		String Descifrado = "";
+		String maserati = "";
 		
-		Texto = Texto.toUpperCase();
+		code = code.toUpperCase();
 		char letra;	
-		for (int Key = 1 ; Key <= 27; Key++) {
-		for (int i = 0; i < Texto.length(); i++) {
-			letra = Texto.charAt(i);
-			int lug = Ab.indexOf(letra);
+		for (int Keyword = 1 ; Keyword <= 27; Keyword++) {
+		for (int i = 0; i < code.length(); i++) {
+			letra = code.charAt(i);
+			int lug = assassins.indexOf(letra);
 		
 		if(lug == -1) {
-			Descifrado += letra;
+			 maserati+= letra;
 		}else {
-			if((lug + Key) >= 27) {
-				Descifrado += Ab.charAt((lug + Key) - 27);
+			if((lug + Keyword) >= 27) {
+				maserati += assassins.charAt((lug + Keyword) - 27);
 			}else {
-				Descifrado += Ab.charAt(lug + Key);
+				maserati += assassins.charAt(lug + Keyword);
 			}	
 		}
 		}
-		System.out.println("Descifrado recorrido " + Key + " Veces da como resultado: " + Descifrado);
+		System.out.println("Descifrado recorrido " + Keyword + " Veces da como resultado: " + maserati);
 		System.out.println("\r\n");
-		Descifrado = "";
+		maserati = "";
 		}
     }
     
